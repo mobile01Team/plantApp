d518513446e0240ed2e3066e7cd4b2c72927f050
seongju1015 <gmrfkels1015@gmail.com>
2024-05-30 10:40:10 +0900

    이미지에서 텍스트 추출 기능 추가

--- plant/lib/search_image.dart
+++ plant/lib/search_image.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
 class SearchImage extends StatefulWidget {
   const SearchImage({super.key});
@@ -8,14 +12,48 @@ class SearchImage extends StatefulWidget {
 }
 
 class _SearchImageState extends State<SearchImage> {
+  String parsedtext = '';
+
+  Future _getFromGallery() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile == null) return;
+
+    var bytes = File(pickedFile.path.toString()).readAsBytesSync();
+    String img64 = base64Encode(bytes);
+
+    var url = 'https://api.ocr.space/parse/image';
+    var payload = {
+      "base64Image": "data:image/jpg;base64,${img64.toString()}",
+      "language": "kor"
+    };
+    var header = {"apikey": "K88781771388957"};
+
+    var post = await http.post(Uri.parse(url), body: payload, headers: header);
+    var result = jsonDecode(post.body);
+
+    setState(() {
+      parsedtext = result['ParsedResults'][0]['ParsedText'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Search'),
       ),
-      body: const Center(
-        child: Text('Search Page Content'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _getFromGallery,
+              child: const Text('Select Image from Gallery'),
+            ),
+            const SizedBox(height: 20),
+            Text(parsedtext.isNotEmpty ? parsedtext : 'No text extracted'),
+          ],
+        ),
       ),
     );
   }
