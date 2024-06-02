dfa26cf16e0492c20fbc595997c180097b888d77
seizethedaylyn <rlacofls294@gmail.com>
2024-06-02 19:07:00 +0900

    위치에 따른 날씨 불러오기

--- plant/ios/Runner/AppDelegate.swift
+++ plant/ios/Runner/AppDelegate.swift
@@ -1,13 +1,16 @@
 import UIKit
 import Flutter
+import GoogleMaps  // Add this import
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyBqkIZjpT3NMzdDLmsDkqLZW8SMFh_UUKM")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
